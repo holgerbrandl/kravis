c79cc9d68a7f9843b65222594a033dd7ee29012d
Holger Brandl <holgerbrandl@users.noreply.github.com>
2018-09-17 11:34:33 +0200

    define exit behavior in swing example

--- src/main/kotlin/kravis/device/SwingExample.kt
+++ src/main/kotlin/kravis/device/SwingExample.kt
@@ -1,8 +1,10 @@
 import javafx.application.Platform
 import javafx.beans.property.SimpleObjectProperty
 import javafx.embed.swing.JFXPanel
+import javafx.event.EventHandler
 import javafx.scene.image.Image
 import javafx.stage.Stage
+import javafx.stage.WindowEvent
 import tornadofx.*
 import javax.swing.SwingUtilities
 
@@ -13,6 +15,12 @@ object SwingApp {
             JFXPanel() // this will prepare JavaFX toolkit and environment
             Platform.runLater {
                 val stage = Stage()
+
+                stage.onCloseRequest = EventHandler<WindowEvent> {
+                    Platform.exit()
+                    System.exit(0)
+                }
+
                 val app = HelloWorldApp()
                 app.start(stage)
             }
